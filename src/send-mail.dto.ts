2f78852a267db30fa5ed1333da09b07dea6376e3
tGeorgiy <107052692+tGeorgiy@users.noreply.github.com>
2023-03-06 16:49:29 +0200

    feat: mail azure function (#1)
    * chore: configuration
    
    * feat: azure

---
+++ src/send-mail.dto.ts
@@ -0,0 +1,23 @@
+import { IsEmail, IsEnum, IsString } from 'class-validator';
+
+export enum TemplateType {
+  VERIFICATION_CODE = 'VERIFICATION_CODE',
+}
+
+export class SendMailDto {
+  constructor(data) {
+    Object.assign(this, data);
+  }
+  @IsEmail()
+  readonly to: string;
+
+  @IsString()
+  readonly subject: string;
+
+  @IsString()
+  readonly text: string;
+
+  @IsString()
+  @IsEnum(TemplateType)
+  readonly templateType: TemplateType;
+}
